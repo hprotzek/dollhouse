bceb4baa8bc1eed863c16650b7d6a2666a001da9
Holger Protzek <holger.protzek@springer.com>
2018-01-13 19:12:38 +0100

    added wifid

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
-#include "Adafruit_TLC5947.h"
+#include <Adafruit_TLC5947.h>
 #include <Wire.h>
 #include <SparkFunSX1509.h>
 #include <Room.h>
-#include "FS.h"
+#include <FS.h>
 #include <FiniteStateMachine.h>
 #include <Wheel.h>
 #include <Button.h>
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include "config.h"
 
 #define NUM_TLC5974 1
 #define DATA        D5
@@ -46,6 +53,13 @@ unsigned long lastModeChange = 0;
 // const int GREEN_PIN = D3;
 // const int RED_PIN   = D4;
 
+void setup_wifi();
+void setup_networking();
+void callback(char* topic, byte* payload, unsigned int length);
+void reconnect();
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 void setup() {
   Serial.begin(9600);
 
@@ -60,10 +74,11 @@ void setup() {
     Serial.println("IO shield start failed");
     // digitalWrite(GREEN_PIN, LOW);
     // digitalWrite(RED_PIN, HIGH);
-    while (1) ;
+    // while (1) ;
+  } else {
+    ioExt.debounceTime(32); // Set debounce time to 32 ms.
+    Serial.println("IO shield start successful");
   }
-  ioExt.debounceTime(32); // Set debounce time to 32 ms.
-  Serial.println("IO shield start successful");
 
   attachInterrupt(digitalPinToInterrupt(D3), nextMode, FALLING);
 
@@ -78,13 +93,113 @@ void setup() {
   //rooms[7].begin("reserve", &ioExt, &ledExt, 7, 7);
   Serial.println("Rooms start initializing successful");
 
+
+  setup_networking();
   // pinMode(RED_PIN, OUTPUT);
   // pinMode(GREEN_PIN, OUTPUT);
   // digitalWrite(GREEN_PIN, LOW);
   // digitalWrite(RED_PIN, LOW);
 }
 
+void setup_networking() {
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+  //OTA SETUP
+  ArduinoOTA.setPort(OTAport);
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname(SENSORNAME);
+
+  // No authentication by default
+  ArduinoOTA.setPassword((const char *)OTApassword);
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Starting");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(SENSORNAME, mqtt_username, mqtt_password)) {
+      Serial.println("connected");
+      // client.subscribe(light_command_topic);
+      // not required or?
+      // setColor(0, 0, 0);
+      // sendState();
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+
+}
+
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    delay(1);
+    Serial.print("WIFI Disconnected. Attempting reconnection.");
+    setup_wifi();
+    return;
+  }
+
+  client.loop();
+
+  ArduinoOTA.handle();
+
   if (modeButton.uniquePress()){
    nextMode();
   }
